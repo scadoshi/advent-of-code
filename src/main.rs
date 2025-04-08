8c2342d701c7889d33c03c9368eec62cf2b4fa47
orangutron <scottyfermo@hotmail.com>
2025-04-08 12:42:18 -0700

    Templates for all years added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod years;
-use crate::years::y2015::mods::day01::{part_one, part_two};
+use crate::years::y2016::mods::day01::{part_one, part_two};
 
 fn main() {
     part_one();
--- src/years/mod.rs
+++ src/years/mod.rs
@@ -1,2 +1,10 @@
 pub mod y2015;
+pub mod y2016;
+pub mod y2017;
+pub mod y2018;
+pub mod y2020;
+pub mod y2021;
+pub mod y2022;
+pub mod y2023;
 pub mod y2024;
+
