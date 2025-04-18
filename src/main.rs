52dc8787e59b22e4c59098c6dd75872f0d26517f
orangutron <scottyfermo@hotmail.com>
2025-04-17 22:25:02 -0700

    Templating out mod.rs for each year

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod years;
-use crate::years::y2023::mods::day03::{part_one, part_two} ;
+use crate::years::y2019::mods::day03::{part_one, part_two} ;
 
 fn main() {
     part_one();
---
+++ src/years/y2019/mods/day03.rs
@@ -0,0 +1,5 @@
+#[allow(dead_code)]
+pub fn part_one() {}
+
+#[allow(dead_code)]
+pub fn part_two() {}
