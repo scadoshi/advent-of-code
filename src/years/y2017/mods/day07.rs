be8b6cbbf1eada1e893891a380f0f5dac4614f94
Scotty Fermo <scotty.fermo@imaginehalo.com>
2025-06-03 16:27:32 -0700

    ...

--- src/years/y2017/mods/day07.rs
+++ src/years/y2017/mods/day07.rs
@@ -1,123 +1,129 @@
-use std::{error::Error, num::ParseIntError, str::FromStr};
+use std::{collections::HashMap, error::Error, fmt::Display, num::ParseIntError, str::FromStr};
+
+#[derive(Debug)]
+enum NodeError {
+    OpenParenthesisNotFound,
+    CloseParenthesisNotFound,
+    NameNotFound,
+    ParseWeightError(ParseIntError),
+}
+
+impl Display for NodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NodeError::OpenParenthesisNotFound => {
+                write!(f, "open parenthesis needed to identify weight")
+            }
+            NodeError::CloseParenthesisNotFound => {
+                write!(f, "close parenthesis needed to identify weight")
+            }
+            NodeError::NameNotFound => {
+                write!(f, "name not found before open parenthesis")
+            }
+            NodeError::ParseWeightError(_) => write!(
+                f,
+                "value found between parenthesis failed to parse to usize"
+            ),
+        }
+    }
+}
+
+impl Error for NodeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            NodeError::ParseWeightError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
 
 #[derive(Debug, Default)]
-struct Program {
-    name: String,
+struct NodeData {
     weight: usize,
     children: Vec<String>,
 }
 
-#[derive(Debug)]
-enum ProgramCreationError {
-    OpenBracketNotFound,
-    CloseBracketNotFound,
-    ParseWeightError(ParseIntError),
-    NameNotFound,
-}
-
-impl std::fmt::Display for ProgramCreationError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ProgramCreationError::OpenBracketNotFound => {
-                write!(f, "open bracket is required to identify weight")
-            }
-            ProgramCreationError::CloseBracketNotFound => {
-                write!(f, "close bracket is required to identify weight")
-            }
-            ProgramCreationError::ParseWeightError(e) => {
-                write!(f, "unable to parse given weight value into usize: {}", e)
-            }
-            ProgramCreationError::NameNotFound => {
-                write!(f, "name str not found before open bracket")
-            }
-        }
-    }
-}
-
-impl Error for ProgramCreationError {
-    fn cause(&self) -> Option<&dyn Error> {
-        None
-    }
-    fn description(&self) -> &str {
-        "error creating program"
-    }
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        None
-    }
-}
-
-impl std::str::FromStr for Program {
-    type Err = Box<dyn std::error::Error>;
+struct Node(String, NodeData);
+impl std::str::FromStr for Node {
+    type Err = Box<dyn Error>;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let obp = s
+        let opp = s
             .find("(")
-            .ok_or_else(|| ProgramCreationError::OpenBracketNotFound)?;
+            .ok_or_else(|| NodeError::OpenParenthesisNotFound)?;
 
-        let cbp = s
-            .find(")")
-            .ok_or_else(|| ProgramCreationError::CloseBracketNotFound)?;
-
-        let name: String = s
-            .chars()
-            .take(obp - 1)
-            .filter(|c| !c.is_whitespace())
-            .collect();
+        let name = s[..opp].trim().to_string();
 
         if name.is_empty() {
-            return Err(Box::new(ProgramCreationError::NameNotFound));
+            return Result::Err(Box::new(NodeError::NameNotFound));
         }
 
-        let weight: usize = s[obp + 1..cbp]
+        let cpp = s
+            .find(")")
+            .ok_or_else(|| NodeError::CloseParenthesisNotFound)?;
+
+        let weight: usize = s[opp + 1..cpp]
             .parse()
-            .map_err(|e| ProgramCreationError::ParseWeightError(e))?;
+            .map_err(|e| NodeError::ParseWeightError(e))?;
 
         let mut children: Vec<String> = Vec::new();
+
         if let Some((_, children_str)) = s.split_once("->") {
             children.extend(
                 children_str
-                    .split(',')
-                    .map(|child_str| child_str.trim().to_string()),
-            );
+                    .split(",")
+                    .map(|x| x.trim().to_string())
+                    .collect::<Vec<String>>(),
+            )
         }
 
-        Ok(Self {
-            name,
-            weight,
-            children,
-        })
+        Ok(Node(name, NodeData { weight, children }))
     }
 }
 
-trait FindRoot {
-    fn find_root(&self) -> Option<&Program>;
-}
-impl FindRoot for Box<[Program]> {
-    fn find_root(&self) -> Option<&Program> {
-        let children: Vec<&String> = self
-            .iter()
-            .filter(|x| !x.children.is_empty())
-            .flat_map(|x| x.children.iter().collect::<Vec<&String>>())
-            .collect();
+type HashNodes = HashMap<String, NodeData>;
 
-        self.iter().find(|x| !children.contains(&&x.name))
+impl FromIterator<Node> for HashNodes {
+    fn from_iter<T: IntoIterator<Item = Node>>(iter: T) -> Self {
+        iter.into_iter()
+            .map(|Node(name, node_data)| (name, node_data))
+            .collect()
+    }
+}
+
+trait Root {
+    fn root(&self) -> Option<String>;
+}
+
+impl Root for HashNodes {
+    fn root(&self) -> Option<String> {
+        let children: Vec<&String> = self
+            .values()
+            .filter(|x| !x.children.is_empty())
+            .flat_map(|x| &x.children)
+            .collect();
+        self.keys()
+            .find(|k| !children.contains(k))
+            .map(|x| x.to_owned())
     }
 }
 
 #[allow(dead_code)]
-pub fn part_one() -> Result<(), Box<dyn std::error::Error>> {
-    let programs: Box<[Program]> = include_str!("../inputs/day07.txt")
-        .lines()
-        .map(|x| Program::from_str(x))
-        .collect::<Result<Box<[Program]>, _>>()?;
-
-    println!("{:#?}", programs.find_root());
+pub fn part_one() -> Result<(), Box<dyn Error>> {
+    let start = std::time::Instant::now();
+    println!(
+        "{:#?}\n...\nruntime={:?}",
+        {
+            let x = include_str!("../inputs/day07.txt")
+                .lines()
+                .map(|s| Node::from_str(s))
+                .collect::<Result<HashNodes, _>>()?;
+            x.root()
+        },
+        start.elapsed()
+    );
 
     Ok(())
 }
-
-// good structure so far
-// now build programs to be a HashMap<String, ProgramContent>
-// for faster lookups
 
 #[allow(dead_code)]
 pub fn part_two() {}
